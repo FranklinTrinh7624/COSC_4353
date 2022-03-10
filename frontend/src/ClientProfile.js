da569d539cd90f9a5aac71ae4c8d9cd2ae5690fe
Franklin Trinh <frankietri2680@gmail.com>
2022-03-09 22:12:33 -0600

    F: Made Login+Profile backend ready S: Prepare connection to front

--- frontend/src/ClientProfile.js
+++ frontend/src/ClientProfile.js
@@ -1,230 +1,159 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ClientProfile.css";
+function Profile() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setStates] = useState("");
+  const [zipcode, setZipcode] = useState("");
 
-const validateForm = (errors) => {
-  let valid = true;
-  Object.values(errors).forEach((val) => {
-    val.length > 0 && (valid = false);
-  });
-  return valid;
-};
+  return (
+    <div className="clientProfile">
+      <div className="form-wrapper">
+        <h1>Client Profile Form</h1>
+        <form onsum>
+          <div className="firstName">
+            <label>First Name: </label>
+            <input
+              type="text"
+              id="first-name"
+              name="firstname"
+              maxLength={25}
+              required
+              onChange={(e) => setFirstName(e.target.value)}
+            ></input>
+          </div>
 
-class Profile extends React.Component {
-  constructor(props) {
-    super(props);
+          <div className="lastName">
+            <label>Last Name: </label>
+            <input
+              type="text"
+              id="last-name"
+              name="lastname"
+              maxLength={25}
+              required
+              onChange={(e) => setLastName(e.target.value)}
+            ></input>
+          </div>
 
-    this.state = {
-      firstName: null,
-      lastName: null,
-      firstAddress: null,
-      secondAddress: null,
-      city: null,
-      state: null,
-      zipcode: null,
-      errors: {
-        firstName: "",
-        lastName: "",
-        firstAddress: "",
-        secondAddress: "",
-        city: "",
-        state: "",
-        zipcode: "",
-      },
-    };
-  }
+          <div className="firstAddress">
+            <label>Address 1: </label>
+            <input
+              type="text"
+              id="addr-1"
+              name="addr1"
+              maxLength={100}
+              required
+              onChange={(e) => setAddress1(e.target.value)}
+            ></input>
+          </div>
+          <div className="secondAddress">
+            <label>Address 2: </label>
+            <input
+              type="text"
+              id="addr-2"
+              name="addr2"
+              maxLength={100}
+              onChange={(e) => setAddress2(e.target.value)}
+            ></input>
+          </div>
+          <div className="city">
+            <label>City: </label>
+            <input
+              type="text"
+              id="city"
+              name="City"
+              maxLength={100}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            ></input>
+          </div>
 
-  handleChange = (event) => {
-    event.preventDefault();
-    const { name, value } = event.target;
-    let errors = this.state.errors;
+          <div className="State">
+            <label>State: </label>
+            <select
+              id="states"
+              name="States"
+              required
+              onChange={(e) => setStates(e.target.value)}
+            >
+              <option disabled selected value>
+                Choose a State
+              </option>
+              <option value="Alabama"> Alabama(AL)</option>
+              <option value="Alaska"> Alaska(AK) </option>
+              <option value="Arizona"> Arizona(AZ)</option>
+              <option value="Arkansas"> Arkansas(AR)</option>
+              <option value="California"> California(CA)</option>
+              <option value="Colorado"> Colorado(CO)</option>
+              <option value="Connecticut"> Connecticut(CT)</option>
+              <option value="Delaware"> Delaware(DE)</option>
+              <option value="Florida"> Florida(FL)</option>
+              <option value="Georgia"> Georgia(GA)</option>
+              <option value="Hawaii"> Hawaii(HI)</option>
+              <option value="Idaho"> Idaho(ID)</option>
+              <option value="Illinois"> Illinois(IL)</option>
+              <option value="Indiana"> Indiana(IN)</option>
+              <option value="Iowa"> Iowa(IA)</option>
+              <option value="Kansas"> Kansas(KS)</option>
+              <option value="Kentuckey"> Kentuckey(KY)</option>
+              <option value="Louisiana"> Louisiana(LA)</option>
+              <option value="Maine"> Maine(ME)</option>
+              <option value="Maryland"> Maryland(MD)</option>
+              <option value="Massachusetts"> Massachusetts(MA)</option>
+              <option value="Michigan"> Michigan(MI)</option>
+              <option value="Minnesota"> Minnesota(MN)</option>
+              <option value="Mississippi"> Mississippi(MS)</option>
+              <option value="Missouri"> Missouri(MO)</option>
+              <option value="Montana"> Montana(MT)</option>
+              <option value="Nebraska"> Nebraska(NE)</option>
+              <option value="Nevada"> Nevada(NV)</option>
+              <option value="New Hampshire"> New Hampshire(NH)</option>
+              <option value="New Jersey"> New Jersey(NJ)</option>
+              <option value="New Mexico"> New Mexico(NM)</option>
+              <option value="New York"> New York(NY)</option>
+              <option value="North Carolina"> North Carolina(NC)</option>
+              <option value="North Dakota"> North Dakota(ND)</option>
+              <option value="Ohio"> Ohio(OH)</option>
+              <option value="Oklahoma"> Oklahoma(OK)</option>
+              <option value="Oregon"> Oregon(OR)</option>
+              <option value="Pennsylvania"> Pennsylvania(PA)</option>
+              <option value="Rhode Island"> Rhode Island(RI)</option>
+              <option value="South Carolina"> South Carolina(SC)</option>
+              <option value="South Dakota"> South Dakota(SD)</option>
+              <option value="Tennessee"> Tennessee(TN)</option>
+              <option value="Texas"> Texas(TX)</option>
+              <option value="Utah"> Utah(UT)</option>
+              <option value="Vermont"> Vermont(VT)</option>
+              <option value="Virginia"> Virginia(VA)</option>
+              <option value="Washington"> Washington(WA)</option>
+              <option value="West Virginia"> West Virginia(WV)</option>
+              <option value="Wisconsin"> Wisconsin(WI)</option>
+              <option value="Wyoming"> Wyoming(WY)</option>
+            </select>
+          </div>
 
-    switch (name) {
-      case "firstName":
-        errors.firstName = value.length > 50 ? "Maximum is 50 characters" : "";
-        break;
-      case "lastName":
-        errors.lastName = value.length > 50 ? "Maximum is 50 characters" : "";
-        break;
-      case "firstAddress":
-        errors.firstAddress =
-          value.length > 100 ? "Maximum is 100 characters" : "";
-        break;
-      case "secondAddress":
-        errors.secondAddress =
-          value.length > 100 ? "Maximum is 100 characters" : "";
-        break;
-      case "city":
-        errors.city = value.length > 100 ? "Maximum is 100 characters" : "";
-        break;
-      case "state":
-        errors.state = value.length < 2 ? "Please select a state" : "";
-        break;
-      case "zipcode":
-        if (value.length < 5) {
-          errors.zipcode = "At least 5 characters required";
-        } else if (value.length > 9) {
-          errors.zipcode = "Maximum is 9 characters";
-        }
-        break;
-      default:
-        break;
-    }
-    this.setState({ errors, [name]: value }, () => console.log(this.state));
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    if (validateForm(this.state.errors)) {
-      console.info("Valid Form - Creating Profile");
-    } else {
-      console.error("Invalid Form");
-    }
-  };
-
-  render() {
-    const { errors } = this.state;
-    return (
-      <div className="clientProfile">
-        <div className="form-wrapper">
-          <h1>Client Profile Form </h1>
-          <form onSubmit={this.handleSubmit} noValidate>
-            <div className="firstName">
-              <label>First Name: </label>
-              <input
-                type="text"
-                name="fName"
-                onChange={this.handleChange}
-                noValidate
-              />
-              {errors.firstName.length > 0 && (
-                <span className="errMsg"> {errors.firstName} </span>
-              )}
-            </div>
-            <div className="lastName">
-              <label>Last Name: </label>
-              <input
-                type="text"
-                name="lName"
-                onChange={this.handleChange}
-                noValidate
-              />
-              {errors.lastName.length > 50 && (
-                <span className="errMsg"> {errors.lastName} </span>
-              )}
-            </div>
-            <div className="firstAddress">
-              <label>Address 1: </label>
-              <input
-                type="text"
-                name="address1"
-                onChange={this.handleChange}
-                noValidate
-              />
-              {errors.firstAddress.length > 100 && (
-                <span className="errMsg"> {errors.firstAddress} </span>
-              )}
-            </div>
-            <div className="secondAddress">
-              <label>Address 2: </label>
-              <input
-                type="text"
-                name="address2"
-                onChange={this.handleChange}
-                noValidate
-              />
-              {errors.secondAddress.length > 100 && (
-                <span className="errMsg"> {errors.secondAddress} </span>
-              )}
-            </div>
-            <div className="city">
-              <label>City: </label>
-              <input
-                type="text"
-                name="city"
-                onChange={this.handleChange}
-                noValidate
-              />
-              {errors.city.length > 100 && (
-                <span className="errMsg"> {errors.city} </span>
-              )}
-            </div>
-            <div className="State">
-              <label>State: </label>
-              <select name="state">
-                <option value="Alabama"> Alabama(AL)</option>
-                <option value="Alaska"> Alaska(AK) </option>
-                <option value="Arizona"> Arizona(AZ)</option>
-                <option value="Arkansas"> Arkansas(AR)</option>
-                <option value="California"> California(CA)</option>
-                <option value="Colorado"> Colorado(CO)</option>
-                <option value="Connecticut"> Connecticut(CT)</option>
-                <option value="Delaware"> Delaware(DE)</option>
-                <option value="Florida"> Florida(FL)</option>
-                <option value="Georgia"> Georgia(GA)</option>
-                <option value="Hawaii"> Hawaii(HI)</option>
-                <option value="Idaho"> Idaho(ID)</option>
-                <option value="Illinois"> Illinois(IL)</option>
-                <option value="Indiana"> Indiana(IN)</option>
-                <option value="Iowa"> Iowa(IA)</option>
-                <option value="Kansas"> Kansas(KS)</option>
-                <option value="Kentuckey"> Kentuckey(KY)</option>
-                <option value="Louisiana"> Louisiana(LA)</option>
-                <option value="Maine"> Maine(ME)</option>
-                <option value="Maryland"> Maryland(MD)</option>
-                <option value="Massachusetts"> Massachusetts(MA)</option>
-                <option value="Michigan"> Michigan(MI)</option>
-                <option value="Minnesota"> Minnesota(MN)</option>
-                <option value="Mississippi"> Mississippi(MS)</option>
-                <option value="Missouri"> Missouri(MO)</option>
-                <option value="Montana"> Montana(MT)</option>
-                <option value="Nebraska"> Nebraska(NE)</option>
-                <option value="Nevada"> Nevada(NV)</option>
-                <option value="New Hampshire"> New Hampshire(NH)</option>
-                <option value="New Jersey"> New Jersey(NJ)</option>
-                <option value="New Mexico"> New Mexico(NM)</option>
-                <option value="New York"> New York(NY)</option>
-                <option value="North Carolina"> North Carolina(NC)</option>
-                <option value="North Dakota"> North Dakota(ND)</option>
-                <option value="Ohio"> Ohio(OH)</option>
-                <option value="Oklahoma"> Oklahoma(OK)</option>
-                <option value="Oregon"> Oregon(OR)</option>
-                <option value="Pennsylvania"> Pennsylvania(PA)</option>
-                <option value="Rhode Island"> Rhode Island(RI)</option>
-                <option value="South Carolina"> South Carolina(SC)</option>
-                <option value="South Dakota"> South Dakota(SD)</option>
-                <option value="Tennessee"> Tennessee(TN)</option>
-                <option value="Texas"> Texas(TX)</option>
-                <option value="Utah"> Utah(UT)</option>
-                <option value="Vermont"> Vermont(VT)</option>
-                <option value="Virginia"> Virginia(VA)</option>
-                <option value="Washington"> Washington(WA)</option>
-                <option value="West Virginia"> West Virginia(WV)</option>
-                <option value="Wisconsin"> Wisconsin(WI)</option>
-                <option value="Wyoming"> Wyoming(WY)</option>
-              </select>
-              {errors.state.length > 0 && (
-                <span className="errMsg"> {errors.state} </span>
-              )}
-            </div>
-            <div className="zipcode">
-              <label>Zipcode: </label>
-              <input
-                type="text"
-                name="zipcode"
-                onChange={this.handleChange}
-                noValidate
-              />
-              {errors.zipcode.length > 5 && (
-                <span className="errMsg"> {errors.zipcode} </span>
-              )}
-            </div>
-            <div className="savebtn">
-              <button>Save</button>
-            </div>
-          </form>
-        </div>
+          <div className="zipcode">
+            <label>Zipcode: </label>
+            <input
+              inputMode="numeric"
+              type="text"
+              id="zipcode"
+              name="Zipcode"
+              maxLength={9}
+              minLength={5}
+              required
+              onChange={(e) => setZipcode(e.target.value)}
+            ></input>
+          </div>
+          <button type="submit" id="save">
+            Save
+          </button>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
 export default Profile;
--- frontend/src/LoginRegistration.js
+++ frontend/src/LoginRegistration.js
@@ -4,6 +4,11 @@ import "./LoginRegistration.css";
 function LoginRegistration() {
   const [userRegister, setUserRegister] = useState("");
   const [passwordRegister, setPasswordRegister] = useState("");
+
+  function handleRegister(e) { /*to be modified?*/
+      e.preventDefault();
+      console.log('You Registered! ' + userRegister + ' ' + passwordRegister);
+  }
   return (
     <div>
       <h1>Login/Registration</h1>
@@ -20,16 +25,18 @@ function LoginRegistration() {
           <button> Login </button>
         </form>
 
-        <form className="register">
+        <form className="register" onSubmit={handleRegister}>
           <label> Registration </label>
           <div className="regiUser">
-            <input type="text" id="user" placeholder="Create Username" />
+            <input type="text" id="user" placeholder="Create Username" 
+            onChange={(e)=> {setUserRegister(e.target.value)}} required/>
           </div>
 
           <div className="regiPass">
-            <input type="password" id="pw" placeholder="Create Password" />
+            <input type="password" id="pw" placeholder="Create Password" 
+            onChange={(e)=> {setPasswordRegister(e.target.value)}} required/>
           </div>
-          <button className="btn"> Register </button>
+          <button className="submit" type="submit"> Register </button>
         </form>
       </div>
     </div>
